49a69a0145f531434309c3b2eaa9fe76804123fc
Ankit <ankitchauhan21500@gmail.com>
2023-06-17 17:47:03 +0530

    op ankit

--- src/newCart/firebase.jsx
+++ src/newCart/firebase.jsx
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import "./index.css"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged,updateProfile, signOut } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, signOut } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFVxCioGK8yRArkJOMGsBQOSo95xdwyeA",
@@ -20,6 +20,7 @@ export default function FirebaseForm() {
   const userEmail = useRef(null);
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const name = e.target.name.value;
@@ -37,36 +38,32 @@ export default function FirebaseForm() {
       toast.error(err.message, { autoClose: 1500 });
     }
   }
-  
+
   const handleLogin = async (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     console.log("this is the login form ");
     const email = e.target.email.value;
     const password = e.target.password.value;
     try {
       const login = await signInWithEmailAndPassword(auth, email, password);
       console.log("this is the login ", login);
-      toast.success("Successfull login bawa!!", {autoClose : 1500})
+      toast.success("Successfull login bawa!!", { autoClose: 1500 })
     }
     catch (error) {
-      toast.error("Invalid Login credentials" , {autoClose: 1500})
+      toast.error("Invalid Login credentials", { autoClose: 1500 })
     }
   }
-  
-  const handleLogout = (e) => {
-    console.log("logout is clicked");
-    signOut(auth);
-    try {
-      toast.success("Sign out is successfull", { autoClose: 1500 });
+
+  const handleLogout = async (e) => {
+    await signOut(auth);
+    toast.success("Sign out is successfull", { autoClose: 1500 });
+    setTimeout(() => {
       window.location.reload();
-    }
-    catch {
-      toast.err("sign out unsuccessfull!!");
-    }
+    }, 1800);
   }
   onAuthStateChanged(auth, (user) => {
     if (user) {
-      console.log("user email-> ", user.email ," \nuser name-> " ,user.displayName )
+      console.log("user email-> ", user.email, " \nuser name-> ", user.displayName)
       userEmail.current.innerHTML = `Email : ${user.email}`;
       userName.current.innerHTML = `Name : ${user.displayName}`;
     } else {
