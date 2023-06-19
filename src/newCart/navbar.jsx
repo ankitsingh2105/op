1a9968537595c1fa0ea4f9a546d7b45d08142a71
Ankit <ankitchauhan21500@gmail.com>
2023-06-19 17:58:18 +0530

    moving to the end

--- src/newCart/navbar.jsx
+++ src/newCart/navbar.jsx
@@ -29,12 +29,12 @@ export default function Navbar() {
       <nav>
         <ToastContainer />
         <ul>
-          <Link to="/" style={{ color: 'black', textDecoration: 'none' }} ><li>Home</li></Link>
+          <Link to="/" style={{ color: 'black' }} ><li>Home</li></Link>
           {
             state ?
-              (<Link to="/login" style={{ color: 'black', textDecoration: 'none' }}><li>Sign In</li></Link>)
+              (<Link to="/login" style={{ color: 'black'}}><li>Sign In</li></Link>)
               :
-              (<li style={{ color: 'black', textDecoration: 'none' }} onClick={handleLogout} >Logout</li>)
+              (<li style={{ color: 'black' }} onClick={handleLogout} >Logout</li>)
           }
         </ul>
       </nav>
