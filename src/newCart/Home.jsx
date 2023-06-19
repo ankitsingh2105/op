1884d46cd16a50dbb9bdbda040a1b6bfc2dadb61
Ankit <ankitchauhan21500@gmail.com>
2023-06-19 13:31:30 +0530

    op

--- src/newCart/Home.jsx
+++ src/newCart/Home.jsx
@@ -1,83 +1,103 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react';
+import { onAuthStateChanged, getAuth, updateProfile } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
-import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { toast } from 'react-toastify';
 import firebaseConfig from './config';
-import dummy from "./dummyimageFirebase.png"
-import { getDoc, doc, getFirestore, onSnapshot } from 'firebase/firestore';
-import { getStorage, ref ,uploadBytes } from "firebase/storage";
+import dummy from './dummyimageFirebase.png';
+
+const app = initializeApp(firebaseConfig);
+
 export default function Home() {
-    const [name, setname] = useState("")
-    const [newDummy , setDummy] = useState(dummy);
-    const infoCenter = useRef(null);
-    // todo : displaying date on the home page
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    const db = getFirestore(app);
-    const storage = getStorage();
+  const [name, setname] = useState('');
+  const [newDummy, setDummy] = useState(dummy);
+  const infoCenter = useRef(null);
+  const [uploadedImage, setImage] = useState(null);
 
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setDummy(user.photoURL);
-            setname(user.displayName);
-                infoCenter.current.innerHTML =
-                    `
-                <br/>
-                <div>Name : ${user.displayName}</div>
-                <br/>
-                <div>Email : ${user.email}</div>
-                <br/>
-                <br/>
-                `
-        } else {
-            console.log("user is out ");
-        }
-    });
-
-
-    // todo : something shit here!!
-    // todo : handeling images 
-
-    const handleImageChanges = (e) => {
-        const photo = e.target.files[0];
-        console.log("this is some files -> "  , e.target.files)
-        console.log("we are selecting an image:", photo.name);
-        const user = auth.currentUser;
-        const storageRef = ref(storage, `images/${user.uid +" - " + user.email}/${photo.name}`);
-      
-        uploadBytes(storageRef, photo).then(() => {
-          console.log('Uploaded a blob or file!');
-        }).catch((error) => {
-          console.log('Error uploading blob or file:', error);
-        });
+      if (user) {
+        setDummy(user.photoURL);
+        setname(user.displayName);
+        infoCenter.current.innerHTML = `
+          <br/>
+          <div>Name: ${user.displayName}</div>
+          <br/>
+          <div>Email: ${user.email}</div>
+          <br/>
+          <br/>
+        `;
+      } else {
+        console.log('user is out');
       }
+    });
+  }, [auth]);
 
-    // todo : getting user info
+  const handleUploading = async () => {
+    if (uploadedImage) {
+      console.log('uploading starts');
+      const user = auth.currentUser;
+      const storageRef = ref(
+        storage,
+        `images/${user.uid + ' - ' + user.email}/${uploadedImage.name}`
+      );
 
-    const getUserInfo = () => {
-        const auth = getAuth();
-        const user = auth.currentUser;
-        if (user) {
-            console.log("current user is  -> ", user);
-        }
+      try {
+        await uploadBytes(storageRef, uploadedImage);
+        console.log('image uploaded to server');
+        const url = await getDownloadURL(storageRef);
+        console.log('url inside the try -> ', url);
+
+        await updateProfile(auth.currentUser, { photoURL: url });
+        toast.success('photo updated', { autoClose: 1500 });
+        window.location.reload();
+      } catch (err) {
+        console.log('Error uploading blob or file:', err);
+        toast.error('Photo not updated', { autoClose: 1500 });
+      }
+    } else {
+      toast.error("no image selected" ,{autoClose:1500})
     }
+  };
 
-    return (
-        <>
-            <div>
-                <h1>Welcome <br />{name} 👋</h1>
-                <div id="info" ref={infoCenter} ></div>
-            </div>
-            <div>
-                <img src={newDummy} alt="" />
-                <br />
-                <div>Profile Image</div>
-                <br />
-                <input type="file" accept='image/*' onChange={handleImageChanges} />
-            </div>
-            <br /><br />
-            <div>
-                <button onClick={getUserInfo}>Get User Info</button>
-            </div>
-        </>
-    )
+  const handleImageChanges = (e) => {
+    const photo = e.target.files[0];
+    setImage(photo);
+    console.log('Selected image:', photo);
+  };
+
+  const getUserInfo = () => {
+    const user = auth.currentUser;
+    if (user) {
+      console.log('Current user:', user);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Welcome</h1>
+        <div id="info" ref={infoCenter}></div>
+      </div>
+      <div>
+        <img src={newDummy} alt="" />
+        <br />
+        <div>Profile Image</div>
+        <br />
+        <input type="file" accept="image/*" onChange={handleImageChanges} />
+        <br />
+        <button onClick={handleUploading}>Upload New Image</button>
+      </div>
+      <br />
+      <br />
+      <div>
+        <button onClick={getUserInfo}>Get User Info</button>
+      </div>
+    </>
+  );
 }
